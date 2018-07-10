1d246280562fd0cf36d22f27e939b2aa1b5f362b
Sean <ussa.say@gmail.com>
2018-07-10 08:47:09 +0800

    Create test-ext2.js

---
+++ test-ext2.js
@@ -0,0 +1,38 @@
+function TestExtension(runtimeProxy) {
+  this.runtime = runtimeProxy;
+  console.log(this);
+}
+
+TestExtension.prototype.getInfo = () => ({
+  id: 'testExt',
+  name: 'test extension',
+  menuIconURI: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAkAAAAFCAAAAACyOJm3AAAAFklEQVQYV2P4DwMMEMgAI/+DEUIMBgAEWB7i7uidhAAAAABJRU5ErkJggg==',
+  blocks: [
+    {
+      opcode: 'testReporter',
+      blockType: 'reporter',
+      text: 'currentTime',
+      arguments: {}
+    },
+    {
+      opcode: 'testReporter2',
+      blockType: 'reporter',
+      text: 'currentDate',
+      arguments: {}
+    }
+  ]
+});
+
+TestExtension.prototype.testReporter = (...otherStuff) => {
+  console.log(otherStuff);
+  console.log(this);
+  return new Date().toTimeString();
+}
+
+TestExtension.prototype.testReporter2 = function(...otherStuff) {
+  console.log(otherStuff);
+  console.log(this);
+  return new Date().toDateString();
+}
+
+Scratch.extensions.register(new TestExtension());
