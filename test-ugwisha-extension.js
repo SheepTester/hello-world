3512b206fe9e6424f127820a50848dbcbcf42917
Sean <ussa.say@gmail.com>
2019-03-03 20:25:46 -0800

    Create test-ugwisha-extension.js

---
+++ test-ugwisha-extension.js
@@ -0,0 +1,16 @@
+UgwishaExtensions.register((() => {
+  const wrapper = createElement('div', {
+    children: [
+      createElement('p', {
+        html: 'DOWN WITH SELF.'
+      }
+    ]
+  });
+
+  return {
+    id: 'test-ugwisha-extension',
+    wrapper: wrapper,
+    name: 'test',
+    icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Flock_of_sheep.jpg/158px-Flock_of_sheep.jpg'
+  };
+})());
