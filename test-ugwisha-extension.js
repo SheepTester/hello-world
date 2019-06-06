a9498ee216a88fe0d2f94c69878dc7d9fc20123d
Sean <ussa.say@gmail.com>
2019-06-05 19:47:36 -0700

    big compactification

--- test-ugwisha-extension.js
+++ test-ugwisha-extension.js
@@ -1,17 +1,5 @@
-UgwishaExtensions.register((() => {
-  const wrapper = createElement('div', {
-    children: [
-      createElement('p', {
-        html: 'DOWN WITH SELF.'
-      })
-    ]
-  });
-
-  return {
-    id: 'test-ugwisha-extension',
-    wrapper: wrapper,
-    name: 'test',
-    icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Flock_of_sheep.jpg/158px-Flock_of_sheep.jpg',
-    url: 'https://sheeptester.github.io/hello-world/test-ugwisha-extension.js'
-  };
-})());
+UgwishaExtensions.register({
+  wrapper: Elem('div', {}, [Elem('p', {innerHTML: 'DOWN WITH SELF.'})]),
+  name: 'test',
+  icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Flock_of_sheep.jpg/158px-Flock_of_sheep.jpg'
+});
