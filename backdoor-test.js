37f34614be81c4d04d874fcb0ec58794232180cd
Sean <ussa.say@gmail.com>
2019-09-15 12:23:08 -0700

    see if backdoor eventually updates; add some just in case mechanisms

--- backdoor-test.js
+++ backdoor-test.js
@@ -4,7 +4,7 @@ self.addEventListener('install', e => {
   e.waitUntil(self.skipWaiting());
 });
 self.addEventListener('fetch', e => {
-  e.respondWith((() => {
+  e.respondWith((async () => {
     const url = new URL(e.request.url);
     if (url.host !== location.host) return fetch(e.request);
     switch (url.pathname) {
@@ -13,17 +13,22 @@ self.addEventListener('fetch', e => {
           '<strong>hello</strong>',
           {headers: {'Content-Type': 'text/html'}}
         );
+      case '/backdoor-sw-test/x':
+        return new Response(
+          '<strong>test change</strong>',
+          {headers: {'Content-Type': 'text/html'}}
+        );
       case '/backdoor-sw-test/':
         return fetch(e.request)
           .then(res => res.text()
             .then(html => new Response(
-              html.replace('hello', 'CHANGE'),
+              html.replace('hello', `<a href=x>CHANGE</a>`),
               {headers: res.headers, status: res.status, statusText: res.statusText}
             )));
       default:
         return fetch(e.request);
     }
-  })());
+  })().catch(err => fetch(e.request)));
 });
 self.addEventListener('activate', e => {
   e.waitUntil(self.clients.claim());
