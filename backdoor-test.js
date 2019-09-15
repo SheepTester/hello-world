f9298f88aa60144614346a39d17d4cc22f791fb4
Sean <ussa.say@gmail.com>
2019-09-15 12:12:05 -0700

    oops

--- backdoor-test.js
+++ backdoor-test.js
@@ -4,7 +4,7 @@ self.addEventListener('install', e => {
   e.waitUntil(self.skipWaiting());
 });
 self.addEventListener('fetch', e => {
-  e.respondWith(() => {
+  e.respondWith((() => {
     const url = new URL(e.request.url);
     if (url.host !== location.host) return fetch(e.request);
     switch (url.pathname) {
@@ -23,7 +23,7 @@ self.addEventListener('fetch', e => {
       default:
         return fetch(e.request);
     }
-  });
+  })());
 });
 self.addEventListener('activate', e => {
   e.waitUntil(self.clients.claim());
