90492e9ad785a831cbe82509f3022c45fc4ffe08
Sean <ussa.say@gmail.com>
2020-05-11 21:26:48 -0700

    for testing; uses localforage too

---
+++ localStorage-extension.js
@@ -0,0 +1,55 @@
+importScripts('https://cdnjs.cloudflare.com/ajax/libs/localforage/1.7.3/localforage.min.js')
+
+class LocalStorage {
+  constructor () {}
+
+  getInfo () {
+    return {
+      id: 'localstorage',
+      name: 'LocalStorage',
+
+      blocks: [
+        {
+          opcode: 'readLocalStorage',
+
+          blockType: Scratch.BlockType.REPORTER,
+
+          text: '[NAME]',
+          arguments: {
+            NAME: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'highscore'
+            }
+          }
+        },
+        {
+          opcode: 'setLocalStorage',
+
+          blockType: Scratch.BlockType.COMMAND,
+
+          text: 'set [NAME] to [VALUE]',
+          arguments: {
+            NAME: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'highscore'
+            },
+            VALUE: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: '12'
+            }
+          }
+        }
+      ]
+    }
+  }
+
+  readLocalStorage ({ NAME }) {
+    return localforage.getItem(NAME)
+  }
+
+  setLocalStorage ({ NAME, VALUE }) {
+    return localforage.setItem(NAME, VALUE)
+  }
+}
+
+Scratch.extensions.register(new LocalStorage())
