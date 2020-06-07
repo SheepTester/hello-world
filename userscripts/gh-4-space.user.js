227cbd8ea8b88cc8fe11c728397f40fb6e8c5b75
Sean <ussa.say@gmail.com>
2020-06-07 13:05:18 -0700

    github four space tab characters

---
+++ userscripts/gh-4-space.user.js
@@ -0,0 +1,13 @@
+/* ==UserStyle==
+@name           Github 4-space tabs
+@namespace      sheeptester.github.io
+@version        1.0.0
+@description    8-space tabs are too fat
+@author         SheepTester
+==/UserStyle== */
+
+@-moz-document domain("github.com") {
+  .tab-size {
+    tab-size: 4 !important;
+  }
+}
