bad94dada730a8f604a5c42b331b039c3cbbb178
SheepTester <ussa.say@gmail.com>
2020-07-02 17:19:42 -0700

    Generate a JS file for descriptions instead

--- audio-to-video/list-audio.sh
+++ audio-to-video/list-audio.sh
@@ -1,4 +1,12 @@
-ls *.{wav,mp3} > audio.txt
+echo "const descriptions = {" > audio-list.js
+for file in *.{wav,mp3}
+do
+  filename=$(basename "$file")
+  echo "  \"${filename}\": \"INSERT_DESC_HERE\"," >> audio-list.js
+done
+echo "}" >> audio-list.js
+echo "// After you add the descriptions, open index.html in your browser." >> audio-list.js
+echo "" >> audio-list.js
 
-echo "Audio file names can be found in audio.txt"
+echo "Insert descriptions for each file in audio-list.js"
 
