80f2c0ff8785b16fd26d1301b43a35bc80526b6e
Sean <ussa.say@gmail.com>
2020-09-06 20:37:27 -0700

    Update skracxteamgxenanto.user.js

--- userscripts/skracxteamgxenanto.user.js
+++ userscripts/skracxteamgxenanto.user.js
@@ -14,7 +14,7 @@
 
   // Scratch usa js-md5
   await import('https://unpkg.com/js-md5@0.7.3/build/md5.min.js')
-  const { md5 } = globalThis
+  const { md5, jQuery: $ } = globalThis
 
   const TAMAÑO_MÁXIMO = 10 * 1000 * 1000 // 10 mb
   const SERVIDOR = 'https://assets.scratch.mit.edu/'
@@ -41,20 +41,28 @@
     return condensados
   }
 
-  async function descargar (condensados) {
+  async function descargar (condensados, progreso) {
     const partes = []
+    let i = 0
     for (const condensado of condensados) {
-      const respuesta = await fetch(SERVIDOR + condensado + '.wav')
+      if (progreso) progreso(i++, condensados.length)
+      const respuesta = await fetch(SERVIDOR + `internalapi/asset/${condensado}.wav/get/`)
       if (!respuesta.ok) {
         throw new Error(await respuesta.text())
       }
       partes.push(await respuesta.blob())
     }
+    if (progreso) progreso(condensados.length, condensados.length)
     return new Blob(partes)
   }
 
   // Hay jQuery en las páginas viejas de Scratch; por qué no usarlo?
-  const indicadorDeProgreso = $('<progress>').hide()
+  const indicadorDeProgreso = $('<progress>')
+  const error = $('<p></p>').css({
+    color: 'red',
+    whiteSpace: 'pre-wrap'
+  }).hide()
+
   const archivoSeleccionado = $('<input type="file">').on('change', function () {
     if (this.files[0] && this.files[0].size > TAMAÑO_MÁXIMO) {
       aviso.show()
@@ -66,10 +74,8 @@
     'Files larger than 10 MB will be split up and uploaded separately. This should be fine, though.'
   ).hide()
   const condensadosGenerados = $('<input type="text" readonly>')
-  const error = $('<p></p>').css({
-    color: 'red',
-    whitespace: 'pre-wrap'
-  }).hide()
+
+  const paraDescargar = $('<input type="text" placeholder="ID">')
 
   $('#tabs-content')
     .css({
@@ -92,7 +98,6 @@
             return
           }
           this.disabled = true
-          indicadorDeProgreso.show()
           const condensados = await subir(archivo, (cargado, total) => {
             indicadorDeProgreso
               .attr('max', total)
@@ -101,15 +106,14 @@
             error.show().text(`Error :(\n${err}`)
           })
           this.disabled = false
-          indicadorDeProgreso.hide()
           if (condensados) {
             // Guardar la extensión del archivo
             if (archivo.name.includes('.') && archivo.name.lastIndexOf('.') > archivo.name.lastIndexOf('/')) {
-              condensados.push(archivo.name.slice(archivo.name.lastIndexOf('.')))
+              condensados.push(archivo.name.slice(archivo.name.lastIndexOf('.') + 1))
             } else {
-              condensados.push('.')
+              condensados.push('')
             }
-            condensadosGenerados.val(condensados.join('.')).focus()
+            condensadosGenerados.val(condensados.join('.')).focus().select()
           }
         }).append(
           $('<label>Select a file to upload: </label>').append(
@@ -121,6 +125,35 @@
         $('<label>Copy and save this ID somewhere to access the file later: </label>')
           .append(condensadosGenerados),
         '<hr>',
+        $('<form></form>').on('submit', async function (e) {
+          e.preventDefault()
+          this.disabled = true
+          error.hide()
+          const condensados = paraDescargar.val().split(/-|\./)
+          const extensión = condensados.pop()
+          const archivo = await descargar(condensados, (cargado, total) => {
+            indicadorDeProgreso
+              .attr('max', total)
+              .attr('value', cargado)
+          }).catch(err => {
+            error.show().text(`Error :(\n${err}`)
+          })
+          this.disabled = false
+          if (archivo) {
+            const lru = URL.createObjectURL(archivo)
+            const enlace = $('<a></a>')
+              .attr('href', lru)
+              .attr('download', condensados.join('-') + (extensión ? '.' + extensión : ''))
+              .appendTo(document.body)
+              .get(0)
+            enlace.click()
+            enlace.remove()
+            URL.revokeObjectURL(lru)
+          }
+        }).append(
+          $('<label>Download file from ID: </label>').append(paraDescargar),
+          $('<input type="submit" value="Download">')
+        ),
         error
       )
     )
