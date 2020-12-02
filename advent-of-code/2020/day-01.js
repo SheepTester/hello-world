c1425daba539c01cbcebc250662d20b1fa64617a
Sean <ussa.say@gmail.com>
2020-12-02 10:01:32 -0800

    advent of code moment
    thanks aaryan this is interesting

---
+++ advent-of-code/2020/day-01.js
@@ -0,0 +1,24 @@
+// Group by last digit because they have to add up to multiple of ten :D
+sets = {}
+document.body.textContent.split('\n').filter(a => a).map(Number).forEach(n => {
+const lastDigit = n % 10
+sets[lastDigit] ||= new Set()
+sets[lastDigit].add(n)
+})
+for (let i = 1; i < 5; i++) {
+  const other = 10 - i
+  for (const a of sets[i]) {
+    for (const b of sets[other]) {
+      if (a + b === 2020) throw [a, b]
+    }
+  }
+}
+for (let i of [0, 5]) {
+  for (const a of sets[i]) {
+    for (const b of sets[i]) {
+      if (a + b === 2020) throw [a, b]
+    }
+  }
+}
+
+1573 * 447
