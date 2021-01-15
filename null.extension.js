cf111c018ed25ec05b618e71b1ddcf3124071c5c
Sean <ussa.say@gmail.com>
2021-01-15 15:24:44 -0800

    Fix brackets, add eval block

--- null.extension.js
+++ null.extension.js
@@ -9,13 +9,29 @@ class Null {
           opcode: 'null',
           blockType: Scratch.BlockType.REPORTER,
           text: 'null'
+        },
+        {
+          opcode: 'eval',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'eval [CODE]',
+          arguments: {
+            CODE: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'undefined'
+            }
+          }
         }
       ]
     }
-    
-    null () {
-      return null
-    }
   }
+    
+  null () {
+    return null
+  }
+
+  eval ({ CODE }) {
+    return eval(CODE)
+  }
+}
   
-  Scratch.extensions.register(new Null())
+Scratch.extensions.register(new Null())
