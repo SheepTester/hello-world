3ce70eba5e62a77eeda9e1de4d38666504c6695d
Sean <ussa.say@gmail.com>
2021-03-28 13:11:54 -0700

    Get all posts from user

--- scrape-ig.js
+++ scrape-ig.js
@@ -1,6 +1,8 @@
 queryTypes = {
   GET_FOLLOWERS: '5aefa9893005572d237da5068082d8d5',
   GET_FOLLOWING: '3dec7e2c57367ef3da3d987d89f9dbc8',
+  HIGHLIGHTS: 'd4d88dc1500312af6f937f7b804c68c3', // and suggested
+  USER_POSTS: '42d2750e44dbac713ff30130659cd891',
 }
 myUserId = '3199863918'
 
@@ -36,3 +38,36 @@ while (true) {
   }
 }
 following
+
+function getPosts (userId, count = 12, cursor = null) {
+  return fetch(
+    'https://www.instagram.com/graphql/query/?' +
+      new URLSearchParams({
+        query_hash: queryTypes.USER_POSTS,
+        variables: JSON.stringify({
+          id: userId,
+          first: count,
+          after: cursor || undefined,
+        })
+      })
+  )
+    .then(r => r.json())
+    .then(({ data: { user: { edge_owner_to_timeline_media } } }) => edge_owner_to_timeline_media)
+}
+
+async function getAllPosts (userId) {
+  const posts = []
+  let cursor
+  while (true) {
+    const { edges, page_info: { end_cursor, has_next_page } } = await getPosts(userId, 1000, cursor)
+    posts.push(...edges)
+    if (has_next_page) {
+      cursor = end_cursor
+    } else {
+      break
+    }
+  }
+  return posts
+}
+
+await getAllPosts('12338962766')
