db311470669bc00cd177a5f22f941c0b7733cd87
SheepTester <ussa.say@gmail.com>
2021-05-18 14:06:47 -0700

    Get source maps from site and get paths

--- deobf/README.md
+++ deobf/README.md
@@ -1,3 +1,5 @@
+## Deobfuscate obfuscated JS files in an app.asar
+
 1. `./get-asar.sh` (only works on Windows, Git Bash 😳)
 
 2. `./extract-asar.sh` to extract app.asar
---
+++ deobf/open-source.ts
@@ -0,0 +1,84 @@
+import { emptyDir } from 'https://deno.land/std@0.97.0/fs/empty_dir.ts'
+
+// From https://github.com/nbuilding/N-lang/blob/22f8a361d141c2e034c732e836f5eb81b57fa935/js/src/utils/type-guards.ts#L23
+function isObjectLike (
+  value: unknown,
+): value is Record<string, unknown> {
+  return typeof value === 'object' && value !== null
+}
+
+function isString (value: unknown): value is string {
+  return typeof value === 'string'
+}
+
+function getSourceMap (json: unknown): Map<string, string> {
+  if (!isObjectLike(json)) {
+    throw new TypeError('JSON is not an object')
+  }
+  const { sources, sourcesContent } = json
+  if (!Array.isArray(sources) || !sources.every(isString)) {
+    throw new TypeError('`sources` is not a string[]')
+  }
+  if (!Array.isArray(sourcesContent) || !sourcesContent.every(isString)) {
+    throw new TypeError('`sourcesContent` is not a string[]')
+  }
+  if (sources.length !== sourcesContent.length) {
+    throw new RangeError('`sources` and `sourcesContent` are not of the same length')
+  }
+  const map = new Map()
+  sources.forEach((sourceUri, i) => {
+    map.set(sourceUri, sourcesContent[i])
+  })
+  return map
+}
+
+async function getSourceMapsFrom (url: string): Promise<Map<string, string>> {
+  const html = await fetch(url)
+    .then(r => r.text())
+
+  const sourceMapUrls = [...html.matchAll(/src="(\/.+?\.js)/g)]
+    .map(([, jsUrl]) => {
+      return new URL(jsUrl + '.map', url)
+    })
+
+  const responses = await Promise.all(
+    sourceMapUrls
+      .map(url => fetch(url)
+        .then(r => r.json())
+        .then(getSourceMap)
+        .catch(() => null))
+  )
+
+  const superMap = new Map()
+
+  for (const map of responses) {
+    if (!map) continue
+    for (const [rawUri, content] of map) {
+      let uri = rawUri
+      if (rawUri.includes('?')) {
+        const searchIndex = rawUri.indexOf('?')
+        uri = rawUri.slice(0, searchIndex)
+        const dotIndex = uri.indexOf('.', uri.lastIndexOf('/'))
+        const search = rawUri.slice(searchIndex + 1)
+        uri = dotIndex === -1
+          ? uri + '__' + search
+          : uri.slice(0, dotIndex) + '__' + search + uri.slice(dotIndex)
+      }
+      if (superMap.has(uri)) {
+        console.warn('Duplicate URI', uri)
+      }
+      superMap.set(uri, content)
+    }
+  }
+
+  return superMap
+}
+
+await Deno.writeTextFile('./url.txt', [...(await getSourceMapsFrom('https://electron.stp-prod.collegeboard.org/')).keys()].join('\n') + '\n')
+// await emptyDir('source')
+// for (const [uri, content] of await getSourceMapsFrom('https://electron.stp-prod.collegeboard.org/')) {
+//   if (!uri.startsWith('webpack:////') && !uri.startsWith('webpack:///../')) {
+//     console.warn(uri, 'doesn\'t start with webpack://')
+//     continue
+//   }
+// }
