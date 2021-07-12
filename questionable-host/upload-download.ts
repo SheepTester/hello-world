7b86e84bc1aeb400a71db01a9742a1eb87dbf6e9
Sean <ussa.say@gmail.com>
2021-07-12 08:40:31 -0700

    questionable-host: Download file, upload w/ credentials

--- questionable-host/upload-download.ts
+++ questionable-host/upload-download.ts
@@ -7,23 +7,41 @@ import { encodeToString } from 'https://deno.land/std@0.95.0/encoding/hex.ts'
 const MAX_SIZE = 10 * 1000 * 1000
 
 /**
- * Size of each chunk of the file to be uploaded to Scratch.
+ * The size of the header, in bytes, at the start of each chunk.
  *
- * This excludes a 24-byte header, which consists of:
+ * It consists of:
  * - 16 bytes for the MD5 hash of the next chunk
  * - 8 bytes representing an u64 of the total file size (excluding the unlinked,
  *   prior chunks)
  */
+const HEADER_SIZE = 24
+
+/**
+ * Size of each chunk of the file to be uploaded to Scratch. This excludes the
+ * 24-byte header.
+ */
 const CHUNK_SIZE = MAX_SIZE - 24
 
-/** Scratch's asset server host */
-const SERVER = 'https://assets.scratch.mit.edu/'
+/**
+ * Scratch's asset server host.
+ *
+ * Decoding from base64 to discourage this file from being picked up by GitHub
+ * search.
+ */
+const SERVER = atob('aHR0cHM6Ly9hc3NldHMuc2NyYXRjaC5taXQuZWR1Lw==')
 
 /**
  * Upload a file to Scratch's asset servers. Returns the md5 hash of the first
  * chunk.
+ *
+ * `scratchSessionSid` is required if uploading outside the browser. Specifying
+ * it in the browser won't do anything because you can't set the cookie header
+ * in front-end JS.
  */
-export async function upload (file: Blob): Promise<string> {
+export async function upload (
+  file: Blob,
+  scratchSessionSid?: string
+): Promise<string> {
   // The last chunk's hash will just be zeroes
   let nextChunkHash = new ArrayBuffer(16)
   // Start from end of chunk backwards because the chunks form a linked list
@@ -36,7 +54,7 @@ export async function upload (file: Blob): Promise<string> {
       // A u64 containing the number of bytes starting from this chunk
       new BigUint64Array([BigInt(file.size - i)]),
       // Chunk data
-      file.slice(byteIndex, byteIndex + CHUNK_SIZE),
+      file.slice(byteIndex, byteIndex + CHUNK_SIZE)
     ])
 
     const md5 = new Md5().update(await blob.arrayBuffer())
@@ -46,6 +64,11 @@ export async function upload (file: Blob): Promise<string> {
     const response = await fetch(SERVER + hash + '.wav', {
       method: 'POST',
       credentials: 'include',
+      headers: scratchSessionSid
+        ? {
+            Cookie: `scratchsessionsid=${scratchSessionSid}`
+          }
+        : {},
       body: blob
     })
     if (!response.ok) {
@@ -56,6 +79,77 @@ export async function upload (file: Blob): Promise<string> {
 }
 
 /**
+ * Download a file from Scratch's asset servers given the md5 hash of the first
+ * chunk.
+ *
+ * `onProgress` is given a value between 0 and 1.
+ */
+export async function download (
+  hash: string,
+  onProgress?: (percent: number) => void
+): Promise<Blob> {
+  if (onProgress) onProgress(0)
+
+  const parts = []
+  let byteCount = 0
+  let totalBytes: number | undefined
+  let nextHash: string | null = hash
+  while (nextHash) {
+    const response = await fetch(
+      `${SERVER}internalapi/asset/${nextHash}.wav/get/`
+    )
+    if (!response.ok) {
+      throw new Error(`HTTP error ${response.status} for ${response.url}`)
+    }
+    if (!response.body) {
+      throw new Error('No response body')
+    }
+
+    const reader = response.body.getReader()
+    const header = new Uint8Array(HEADER_SIZE)
+    let headerPos = 0
+    while (true) {
+      const result = await reader.read()
+      if (result.done) break
+
+      const bytes = result.value
+      parts.push(bytes)
+      byteCount += bytes.length
+
+      // Copy bytes into the header
+      if (headerPos < header.length) {
+        header.set(bytes.slice(0, header.length - headerPos), headerPos)
+        headerPos += bytes.length
+        if (headerPos >= 16) {
+          nextHash = encodeToString(header.slice(0, 16))
+        }
+        if (headerPos >= header.length) {
+          const view = new DataView(header.buffer)
+          const bytesLeft = Number(view.getBigUint64(16))
+          // If this is the first chunk, then the bytes left should be the total
+          // size of the file
+          if (!totalBytes) {
+            totalBytes = bytesLeft
+          }
+          // Determine whether this is the last chunk
+          if (bytesLeft <= CHUNK_SIZE) {
+            nextHash = null
+          }
+        }
+      }
+
+      if (onProgress && totalBytes) {
+        onProgress(byteCount / totalBytes)
+      }
+    }
+  }
+
+  if (onProgress) onProgress(1)
+
+  return new Blob(parts)
+}
+
+/**
  * Download a file uploaded using the old userscript,
  * https://github.com/SheepTester/hello-world/blob/master/userscripts/skracxteamgxenanto.user.js
  *
@@ -63,7 +157,7 @@ export async function upload (file: Blob): Promise<string> {
  */
 export async function downloadOld (
   hashes: string[],
-  onProgress?: (progress: number) => void
+  onProgress?: (percent: number) => void
 ): Promise<Blob> {
   const parts = []
   let i = 0
