a5c184d793b7bcaf65d2216d20ac470582616a8b
Sean <ussa.say@gmail.com>
2021-12-06 15:11:22 -0800

    stream-file: Apparently Deno.File.write does not write all the bytes >:(

--- test/stream-to-file-ws.ts
+++ test/stream-to-file-ws.ts
@@ -4,10 +4,30 @@
 
 import { serve } from 'https://deno.land/std@0.117.0/http/server.ts'
 import { ensureDir } from 'https://deno.land/std@0.117.0/fs/ensure_dir.ts'
+import { writeAll } from 'https://deno.land/std@0.117.0/streams/conversion.ts'
 
 const encoder = new TextEncoder()
 
 await ensureDir('ignored')
+
+function websocketToStream (
+  ws: WebSocket
+): ReadableStream<string | ArrayBuffer> {
+  return new ReadableStream({
+    start (controller) {
+      ws.addEventListener('message', ({ data }) => {
+        if (typeof data === 'string' || data instanceof ArrayBuffer) {
+          controller.enqueue(data)
+        } else {
+          throw new TypeError('Expected a Uint8Array or a string.')
+        }
+      })
+      ws.addEventListener('close', () => {
+        controller.close()
+      })
+    }
+  })
+}
 
 console.log(`HTTP webserver running. Access it at: http://localhost:8080/`)
 await serve(
@@ -22,22 +42,20 @@ await serve(
     }
     try {
       const { socket, response } = Deno.upgradeWebSocket(request)
-      // Overwrite existing file
-      await Deno.writeFile(`./ignored/${path}`, new Uint8Array())
-      const file = await Deno.open(`./ignored/${path}`, {
-        append: true
-      })
-      socket.addEventListener('message', ({ data }) => {
-        if (data instanceof ArrayBuffer) {
-          file.write(new Uint8Array(data))
-        } else if (typeof data === 'string') {
-          file.write(encoder.encode(data))
-        } else {
-          console.error(data)
-          throw new TypeError('Expected a Uint8Array.')
+      socket.addEventListener('open', async () => {
+        // Overwrite existing file
+        await Deno.writeFile(`./ignored/${path}`, new Uint8Array())
+        const file = await Deno.open(`./ignored/${path}`, {
+          append: true
+        })
+        for await (const message of websocketToStream(socket)) {
+          // file.write does not write all bytes >_<
+          if (message instanceof ArrayBuffer) {
+            await writeAll(file, new Uint8Array(message))
+          } else {
+            await writeAll(file, encoder.encode(message))
+          }
         }
-      })
-      socket.addEventListener('close', () => {
         file.close()
       })
       // response.headers.append('Access-Control-Allow-Origin', '*')
