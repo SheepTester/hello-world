8746adbc1f7834048e6e1e61e259552474cdf521
Sean <ussa.say@gmail.com>
2021-12-17 02:04:56 -0800

    Maybe I should discard entries with large distances at some point

--- advent-of-code/2021/day-15/src/main.rs
+++ advent-of-code/2021/day-15/src/main.rs
@@ -13,6 +13,7 @@ where
 
     let mut row = 0;
     let mut column = 0;
+    let mut iterations = 0;
     loop {
         let distance = *distances
             .get(&(row, column))
@@ -53,19 +54,30 @@ where
         }
         visited.insert((row, column));
 
-        let ((next_row, next_column), _) = distances
+        let ((next_row, next_column), d) = distances
             .iter()
             .filter(|(position, _)| !visited.contains(position))
             .min_by_key(|(_, distance)| *distance)
             .expect("There are no unvisited positions.");
         row = *next_row;
         column = *next_column;
+
+        iterations += 1;
+        if iterations > 10000 {
+            iterations = 0;
+            println!(
+                "`distances` now has a size of {} (min distance {})",
+                distances.len(),
+                d
+            );
+        }
     }
 }
 
 type RiskMap = Vec<Vec<u32>>;
 
 fn part_1(map: RiskMap) {
+    println!("Part 1");
     println!(
         "{}",
         dijkstras_algorithm(
@@ -77,6 +89,7 @@ fn part_1(map: RiskMap) {
 }
 
 fn part_2_naive(map: RiskMap) {
+    println!("Part 2 naïve");
     println!(
         "{}",
         dijkstras_algorithm(
@@ -92,6 +105,35 @@ fn part_2_naive(map: RiskMap) {
     );
 }
 
+fn part_2_cached(map: RiskMap) {
+    // Naïve approach but caches map in larger vector rather than calculating it
+    // on the fly
+    println!("Part 2 naïve (with cached new map)");
+    let large_map: RiskMap = (0..5)
+        .flat_map(|row_offset| {
+            map.iter()
+                .map(|row| {
+                    (0..5)
+                        .flat_map(|column_offset| {
+                            row.iter()
+                                .map(|risk| (risk + row_offset + column_offset - 1) % 9 + 1)
+                                .collect::<Vec<u32>>()
+                        })
+                        .collect::<Vec<u32>>()
+                })
+                .collect::<RiskMap>()
+        })
+        .collect();
+    println!(
+        "{}",
+        dijkstras_algorithm(
+            |row: usize, column: usize| large_map[row][column],
+            large_map.len() - 1,
+            large_map[0].len() - 1
+        )
+    );
+}
+
 fn main() {
     let map = include_str!("../../day-15-input.txt")
         .lines()
@@ -102,8 +144,9 @@ fn main() {
         })
         .collect::<Vec<Vec<_>>>();
 
-    match args().next().as_ref().map(|str| str.as_str()) {
+    match args().skip(1).next().as_ref().map(|str| str.as_str()) {
         Some("part-2-naive") => part_2_naive(map),
+        Some("part-2-cached") => part_2_cached(map),
         _ => part_1(map),
     }
 }
