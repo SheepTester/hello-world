8bae8ae9125ebefff4ff78fc7222fd30510cb33e
Sean <ussa.say@gmail.com>
2022-10-02 11:55:06 -0700

    hmm idk

--- src/main.rs
+++ src/main.rs
@@ -1,98 +1,32 @@
-// Definition for a binary tree node.
-#[derive(Debug, PartialEq, Eq)]
-pub struct TreeNode {
-    pub val: i32,
-    pub left: Option<Rc<RefCell<TreeNode>>>,
-    pub right: Option<Rc<RefCell<TreeNode>>>,
-}
-
-impl TreeNode {
-    #[inline]
-    pub fn new(val: i32) -> Self {
-        TreeNode {
-            val,
-            left: None,
-            right: None,
-        }
-    }
-
-    pub fn from(
-        val: i32,
-        left: Option<Rc<RefCell<TreeNode>>>,
-        right: Option<Rc<RefCell<TreeNode>>>,
-    ) -> Option<Rc<RefCell<Self>>> {
-        Some(Rc::new(RefCell::new(TreeNode { val, left, right })))
-    }
-}
 struct Solution;
 
-use std::cell::RefCell;
-use std::collections::VecDeque;
-use std::rc::Rc;
 impl Solution {
-    // fn min_depth_x(root: &TreeNode, depth: i32, smaller: i32) -> i32 {
-    //     if root.left.is_none() && root.right.is_none() {
-    //         return depth;
-    //     }
-    //     let left_min_depth = if let Some(left) = root.left {
-    //         Solution::min_depth_x(root.left, depth + 1, smaller)
-    //     } else {
-
-    //     }
-    // }
-    pub fn min_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-        let mut queue: VecDeque<(i32, Rc<RefCell<TreeNode>>)> = VecDeque::from([(
-            1,
-            if let Some(w) = root {
-                w
-            } else {
-                return 0;
-            },
-        )]);
-        // find first node with no children
-        while let Some((depth, node)) = queue.pop_front() {
-            let lol = node.borrow();
-            if lol.left.is_none() && lol.right.is_none() {
-                return depth;
-            }
-            if let Some(a) = lol.left.clone() {
-                queue.push_back((depth + 1, a));
-            }
-            if let Some(a) = lol.right.clone() {
-                queue.push_back((depth + 1, a));
-            }
+    /// You have `n` dice and each die has `k` faces numbered from 1 to `k`.
+    ///
+    /// Given three integers `n`, `k`, and `target`, return the number of
+    /// possible ways (out of the `k`^`n` total ways) to roll the dice so the
+    /// sum of the face-up numbers equals target. Since the answer may be too
+    /// large, return it **modulo** 10^9 + 7.
+    pub fn num_rolls_to_target(n: i32, k: i32, target: i32) -> i32 {
+        if n * k < target || target < n {
+            return 0;
         }
-        panic!("i dont think this should happen lmao")
+        if n <= 1 {
+            return if k <= target { 1 } else { 0 };
+        }
+        let mut count = 0;
+        for i in 1..=k {
+            count += Solution::num_rolls_to_target(n - 1, k, target - i);
+        }
+        count
     }
 }
 
 fn main() {
+    println!("{:?} = 1", Solution::num_rolls_to_target(1, 6, 3));
+    println!("{:?} = 6", Solution::num_rolls_to_target(2, 6, 7));
     println!(
-        "{:?}",
-        Solution::min_depth(TreeNode::from(
-            3,
-            TreeNode::from(9, None, None),
-            TreeNode::from(
-                20,
-                TreeNode::from(15, None, None),
-                TreeNode::from(7, None, None)
-            )
-        ))
-    );
-    println!(
-        "{:?}",
-        Solution::min_depth(TreeNode::from(
-            2,
-            None,
-            TreeNode::from(
-                3,
-                None,
-                TreeNode::from(
-                    4,
-                    None,
-                    TreeNode::from(5, None, TreeNode::from(6, None, None))
-                )
-            )
-        ))
+        "{:?} = 222616187",
+        Solution::num_rolls_to_target(30, 30, 500)
     );
 }
--- test/listener.ts
+++ test/listener.ts
@@ -1,3 +1,5 @@
+// deno run -A test/listener.ts test/adminbot.html
+
 const server = Deno.listen({ port: 8080 })
 console.log(`HTTP webserver running.  Access it at:  http://localhost:8080/`)
 
@@ -8,25 +10,36 @@ for await (const conn of server) {
   serveHttp(conn)
 }
 
+async function serve (requestEvent: Deno.RequestEvent) {
+  console.log(requestEvent.request.url)
+  const root =
+    Deno.args[0] && new URL(requestEvent.request.url).pathname === '/'
+
+  // The native HTTP server uses the web standard `Request` and `Response`
+  // objects.
+  const body = root
+    ? await Deno.readTextFile(Deno.args[0])
+    : `Your user-agent is:\n\n${
+        requestEvent.request.headers.get('user-agent') ?? 'Unknown'
+      }`
+  // The requestEvent's `.respondWith()` method is how we send the response
+  // back to the client.
+  requestEvent.respondWith(
+    new Response(body, {
+      status: 200,
+      headers: {
+        'Content-Type': 'text/html'
+      }
+    })
+  )
+}
+
 async function serveHttp (conn: Deno.Conn) {
   // This "upgrades" a network connection into an HTTP connection.
   const httpConn = Deno.serveHttp(conn)
   // Each request sent over the HTTP connection will be yielded as an async
   // iterator from the HTTP connection.
   for await (const requestEvent of httpConn) {
-    console.log(requestEvent.request.url)
-
-    // The native HTTP server uses the web standard `Request` and `Response`
-    // objects.
-    const body = `Your user-agent is:\n\n${
-      requestEvent.request.headers.get('user-agent') ?? 'Unknown'
-    }`
-    // The requestEvent's `.respondWith()` method is how we send the response
-    // back to the client.
-    requestEvent.respondWith(
-      new Response(body, {
-        status: 200
-      })
-    )
+    serve(requestEvent)
   }
 }
