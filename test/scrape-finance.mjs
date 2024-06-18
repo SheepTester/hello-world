7e6932f575ada2dd20388688dd956db63b85e1a1
Sean <ussa.say@gmail.com>
2024-06-17 22:20:40 -0700

    scrape https://finance.ucsd.edu/Home/ListFunded

---
+++ test/scrape-finance.mjs
@@ -0,0 +1,25 @@
+import fs from 'fs/promises'
+
+console.error('set term')
+const cookie = await fetch('https://finance.ucsd.edu/Home/UpdateTerm', {
+  method: 'POST',
+  headers: {
+    'content-type': 'application/x-www-form-urlencoded',
+    referer: 'https://finance.ucsd.edu/Home/ListFunded'
+  },
+  redirect: 'manual',
+  body: 'FinanceTerm=1021'
+}).then(r =>
+  r.headers
+    .getSetCookie()
+    .map(cookie => cookie.split('; path=')[0])
+    .join('; ')
+)
+console.error('get table', cookie)
+const html = await fetch('https://finance.ucsd.edu/Home/ListFunded', {
+  headers: { cookie, referer: 'https://finance.ucsd.edu/Home/ListFunded' }
+}).then(r => r.text())
+console.error(html.length)
+await fs.writeFile('/home/sheep/fin.html', html)
+// console.log(html)
+// process.exit()
