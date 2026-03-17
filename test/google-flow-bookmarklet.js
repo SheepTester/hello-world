51ea18361feb1161d40bcf1d1ba58258f06cfe2d
Sean Yen <ussa.say@gmail.com>
2026-03-16 21:24:56 -0700

    bookmarklet for downloading/copyingAdd google-flow-bookmarklet.js for link handling

---
+++ test/google-flow-bookmarklet.js
@@ -0,0 +1,31 @@
+javascript:{
+ const data=   window.__data246653 ??= new WeakSet();
+document.onpointerdown = () => {
+   for (const link of document.querySelectorAll ('[data-discover="true"]')) {
+if (data.has(link)) continue;
+       const url = new URL(link.querySelector('img,video').src);
+       link.href = url;
+                                const wow=[...link.nextElementSibling.querySelectorAll('i + div')].at(-1).textContent;
+                                if (!wow) {console.warn(link);;}
+       link.onclick =async e =>{ e.stopPropagation();
+e.preventDefault();
+            const blob = await fetch(url).then(r => r.blob()) 
+                         ;       link.href=URL.createObjectURL(blob)
+                const h = ()=>(link.style.filter=`hue-rotate(${Math.random()*300+30}deg)`);h();
+                            ;    link.click()
+                              ;  link.onclick=e=>(e.stopPropagation(),h());
+                          }
+   ;    link.download = `${wow} ${url.searchParams.get('name')}`
+    ;   data.add(link)
+   }
+      for (const link of document.querySelectorAll ('.reuse-prompt-button')) {
+if (data.has(link)) continue
+      ;    let btn = link.parentNode.previousElementSibling
+       ;   btn.onclick=e =>{/* e.stopPropagation()*/
+navigator.clipboard.writeText(btn.textContent)
+        ;      btn.style.color = `hsl(${Math.random()*360},50\x25,50\x25)`
+                           }
+       ;   data.add(link)
+      }
+}
+}
