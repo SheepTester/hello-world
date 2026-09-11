9ab9c60200f374e8280a1b85002980ee3bf5cc33
sean (costco asus vivobook) <ussa.say@gmail.com>
2026-09-10 23:47:10 -0700

    compact context (not in the llm sense)

--- test/ios-autofocus/sw.js
+++ test/ios-autofocus/sw.js
@@ -2,6 +2,8 @@
 
 const prefixPromise = fetch('prefix.txt').then(r => r.text())
 const dataPromise = fetch('data.json').then(r => r.json())
+const context =
+  '\n    <p>\n      context: ios safari keeps autofocusing my input for\n      <a href="https://sheeptester.github.io/javascripts/totp.html">totp</a>\n    '
 
 self.addEventListener('fetch', e => {
   const k = new URL(e.request.url).searchParams.get('k')
@@ -9,9 +11,12 @@ self.addEventListener('fetch', e => {
     e.respondWith(
       Promise.all([dataPromise, prefixPromise]).then(async ([data, prefix]) => {
         if (data[k]) {
-          return new Response(data[k].replace('{PREFIX}', prefix), {
-            headers: { 'content-type': 'text/html' }
-          })
+          return new Response(
+            data[k].replace('{CONTEXT}', context).replace('{PREFIX}', prefix),
+            {
+              headers: { 'content-type': 'text/html' }
+            }
+          )
         } else {
           return new Response(
             prefix + `<p><code>${k}</code> not found. <a href='?'>back</a></p>`,
